d5a7654a4e191c10ace762a3bda85a19cf3bfae2
John Carveth <jlcarveth@gmail.com>
2020-06-28 16:53:12 -0400

    URL change johncarveth.com > jlcarveth.dev

--- cst8238/lab5/menu.php
+++ cst8238/lab5/menu.php
@@ -8,22 +8,22 @@
 <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="https://johncarveth.com">Home</a>
+                <a class="nav-link" href="https://jlcarveth.dev">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="https://johncarveth.com/cst8238/lab1/">Lab 1</a>
+                <a class="nav-link" href="https://jlcarveth.dev/cst8238/lab1/">Lab 1</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="https://johncarveth.com/cst8238/lab2/">Lab 2</a>
+                <a class="nav-link" href="https://jlcarveth.dev/cst8238/lab2/">Lab 2</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="https://johncarveth.com/cst8238/lab3/">Lab 3</a>
+                <a class="nav-link" href="https://jlcarveth.dev/cst8238/lab3/">Lab 3</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="https://johncarveth.com/cst8238/lab4/">Lab 4</a>
+                <a class="nav-link" href="https://jlcarveth.dev/cst8238/lab4/">Lab 4</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link active" href="https://johncarveth.com/cst8238/lab5">Lab 5
+                <a class="nav-link active" href="https://jlcarveth.dev/cst8238/lab5">Lab 5
                     <span class="sr-only">(current)</span></a>
             </li>
         </ul>
